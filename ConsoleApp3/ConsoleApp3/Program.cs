3ff086f2f7db7c3a4aac42ab5c9c2fef26ba6371
maksimVAs73 <traffikingvraj@gmail.com>
2024-11-24 12:56:08 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class ArrayMethods
+    {
+        // Сортировка по возрастанию
+        public static int[] SortAscending(int[] arr)
+        {
+            return arr.OrderBy(x => x).ToArray();
+        }
+
+        // Сортировка по убыванию
+        public static int[] SortDescending(int[] arr)
+        {
+            return arr.OrderByDescending(x => x).ToArray();
+        }
+
+        // Объединение двух массивов
+        public static int[] ConcatenateArrays(int[] arr1, int[] arr2)
+        {
+            return arr1.Concat(arr2).ToArray();
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] array1 = { 5, 2, 9, 1, 5, 6 };
+            int[] array2 = { 3, 8, 4, 7, 2 };
+
+            Console.WriteLine("Исходный массив:");
+            Console.WriteLine(string.Join(", ", array1));
+
+            Console.WriteLine("\nОтсортированный по возрастанию:");
+            int[] sortedAsc = SortAscending(array1);
+            Console.WriteLine(string.Join(", ", sortedAsc));
+
+            Console.WriteLine("\nОтсортированный по убыванию:");
+            int[] sortedDesc = SortDescending(array1);
+            Console.WriteLine(string.Join(", ", sortedDesc));
+
+            Console.WriteLine("\nОбъединенные массивы:");
+            int[] concatenated = ConcatenateArrays(array1, array2);
+            Console.WriteLine(string.Join(", ", concatenated));
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class AbsoluteDifference
+    {
+        public static int CalculateDifference(int n)
+        {
+            int baseNumber = 123;
+            int absoluteDifference = Math.Abs(n - baseNumber);
+
+            if (n > baseNumber)
+            {
+                return absoluteDifference * 3;
+            }
+            else
+            {
+                return absoluteDifference;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int n))
+            {
+                int difference = CalculateDifference(n);
+                Console.WriteLine($"Абсолютная разность между {n} и 123: {difference}");
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод.");
+            }
+        }
+    }
+}
